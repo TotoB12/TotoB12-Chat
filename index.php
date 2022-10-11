93a296c8d5109f0f4239d6a7b76440acc9679646
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2022-10-11 14:11:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ session_start();
 if(isset($_GET['logout'])){    
      
     //Simple exit message
-    $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>". $_SESSION['name'] ."</b> has left the chat session.</span><br></div>";
+    $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>". $_SESSION['name'] ."</b> has left the chat.</span><br></div>";
     file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
      
     session_destroy();
