4421ae8b7ec7752fce5cfa793a02d265b17df0f9
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2022-10-11 14:04:38 -0400

    Add files via upload

--- post.php
+++ post.php
@@ -2,28 +2,16 @@
 session_start();
 if(isset($_SESSION['name'])){
      
-    $badWords = array(
-         'fuck' => 'f*ck',
-         'shit' => 'sh*t',
-	 'hell' => 'h*ll',
-         'jesus' => 'j*sus',
-         'god' => 'g*d',
-         'anus' => '*n*s',
-         'ass' => '*ss',
-         'nazi' => 'n*z*',
-         'hitler' => 'h*tl*r',
-         'sex' => 's*x',
-         'porn' => 'p*rn',
-         'dick' => 'd*ck',
-         'penis' => 'p*n*s',
-         'cock' => 'c*ck',
-         'clit' => 'cl*t',
-         'vagina' => 'v*g*n*',
-         'pussy' => 'p*ssy',
-         'trump' => 'tr*mp',
-         'badly' => '***');
+    function censor($sentence, $txtFile) {
+        $words = file($txtFile);
+        foreach ($words as $word) {
+            $word = trim($word);
+            $sentence = str_replace($word, str_repeat("*", strlen($word)), $sentence);
+        }
+        return $sentence;
+    }
 
-    $text = strtr(strtolower($_POST['text']), $badWords);
+    $text = censor(strtolower($_POST['text']), "bad.txt");
 
     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
