83679380fd83db77a3712d9aed204147a7d23d01
Antonin Beliard <91705868+TotoB12@users.noreply.github.com>
2022-10-14 08:37:22 -0400

    Update post.php

--- post.php
+++ post.php
@@ -12,8 +12,17 @@ if(isset($_SESSION['name'])){
     }
 
     $text = censor(strtolower($_POST['text']), "bad.txt");
+     
+    if($text != ""){
+         
+        $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+        file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+         
+    }
+    else{
+        echo '<span class="error">Please type in a message</span>';
+    }
 
-    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
-    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+
 }
 ?>
