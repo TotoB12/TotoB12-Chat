a65a6ae6b622646dc212db5a43ee364cb0512d77
Antonin Beliard <91705868+TotoB12@users.noreply.github.com>
2022-10-13 20:48:31 -0400

    Update post.php

--- post.php
+++ post.php
@@ -2,26 +2,18 @@
 session_start();
 if(isset($_SESSION['name'])){
      
-    if($_POST['text'] != ""){
-         
-        function censor($sentence, $txtFile) {
+    function censor($sentence, $txtFile) {
         $words = file($txtFile);
         foreach ($words as $word) {
             $word = trim($word);
             $sentence = substr(str_replace($word, str_repeat("*", strlen($word)), $sentence), 0, 1000000000);
         }
         return $sentence;
-             
-        $text = censor(strtolower($_POST['text']), "bad.txt");
-
-        $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
-        file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
-    }
-        
-    }
-    else{
-        echo '<span class="error">Please type in a message</span>';
     }
 
+    $text = censor(strtolower($_POST['text']), "bad.txt");
+
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
