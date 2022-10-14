750fca7175eba6c82bdb73c128e5d54123d6873e
Antonin Beliard <91705868+TotoB12@users.noreply.github.com>
2022-10-14 08:42:50 -0400

    Update post.php

--- post.php
+++ post.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['name'])){
 
     $text = censor(strtolower($_POST['text']), "bad.txt");
      
-    if($text != ""){
+    if(($text != "") && ($text != " ")){
          
         $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
         file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
